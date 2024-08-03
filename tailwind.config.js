2f7b714d2fd658782983a576aa337361672f6738
Siddharth <sahusidd715@gmail.com>
2024-08-03 13:56:07 +0530

    modified:   public/stylesheets/output.css 	modified:   tailwind.config.js 	modified:   views/index.ejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./views/index.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '10vw': '10vw',
+        '20vw': '20vw',
+        '30vw': '30vw',
+        '40vw': '40vw',
+        '50vw': '50vw',
+        '60vw': '60vw',
+        '70vw': '70vw',
+        '80vw': '80vw',
+        '90vw': '90vw',
+        '100vw': '100vw',
+      },
+    },
   },
   plugins: [],
 }
